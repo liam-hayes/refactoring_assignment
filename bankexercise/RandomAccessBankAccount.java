0ef01c764dc9a0967f4ed9db0782b292a79636db
liam-hayes <c14763045@mydit.ie>
2018-02-23 15:54:08 +0000

    Clean up imports, remove unused code, make variables private

--- bankexercise/RandomAccessBankAccount.java
+++ bankexercise/RandomAccessBankAccount.java
@@ -1,22 +1,20 @@
 package bankexercise;
 
-
-import java.io.RandomAccessFile;
-import java.io.IOException;
+import java.io.*;
 
 public class RandomAccessBankAccount extends BankAccount {
 	public static final int SIZE = 140;
-	
-	public RandomAccessBankAccount(){
+
+	public RandomAccessBankAccount() {
 		this(0, "", "", "", "", 0.0, 0.0);
 	}
-	
-	public RandomAccessBankAccount(int accountID, String accountNumber, String firstName, String surname, String accountType,
-			double balance, double overdraft){
+
+	public RandomAccessBankAccount(int accountID, String accountNumber, String firstName, String surname,
+			String accountType, double balance, double overdraft) {
 		super(accountID, accountNumber, firstName, surname, accountType, balance, overdraft);
 	}
-	
-	public void read(RandomAccessFile file) throws IOException{
+
+	public void read(RandomAccessFile file) throws IOException {
 		setAccountID(file.readInt());
 		setAccountNumber(readName(file));
 		setFirstName(readName(file));
@@ -25,18 +23,18 @@ public class RandomAccessBankAccount extends BankAccount {
 		setBalance(file.readDouble());
 		setOverdraft(file.readDouble());
 	}
-	
-	private String readName(RandomAccessFile file) throws IOException{
+
+	private String readName(RandomAccessFile file) throws IOException {
 		char name[] = new char[15], temp;
-		for(int count = 0; count<name.length; count++){
+		for (int count = 0; count < name.length; count++) {
 			temp = file.readChar();
 			name[count] = temp;
 		}
-		
+
 		return new String(name).replace('\0', ' ');
 	}
-	
-	public void write(RandomAccessFile file) throws IOException{
+
+	public void write(RandomAccessFile file) throws IOException {
 		file.writeInt(getAccountID());
 		writeName(file, getAccountNumber());
 		writeName(file, getFirstName());
@@ -45,17 +43,16 @@ public class RandomAccessBankAccount extends BankAccount {
 		file.writeDouble(getBalance());
 		file.writeDouble(getOverdraft());
 	}
-	
-	private void writeName(RandomAccessFile file, String name) throws IOException{
+
+	private void writeName(RandomAccessFile file, String name) throws IOException {
 		StringBuffer buffer = null;
-		
-		if(name!=null)
+
+		if (name != null)
 			buffer = new StringBuffer(name);
 		else
 			buffer = new StringBuffer(15);
-		
+
 		buffer.setLength(15);
 		file.writeChars(buffer.toString());
 	}
-			
 }
